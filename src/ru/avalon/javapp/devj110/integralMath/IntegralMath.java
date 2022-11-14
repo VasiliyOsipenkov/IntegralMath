81d5f44695deab6f77043865fd3bbf08f49d913c
Vasiliy Osipenkov <vasiliy.osipenkov@zyfra.com>
2022-11-14 14:50:31 +0300

    final

--- src/ru/avalon/javapp/devj110/integralMath/IntegralMath.java
+++ src/ru/avalon/javapp/devj110/integralMath/IntegralMath.java
@@ -2,78 +2,6 @@ package ru.avalon.javapp.devj110.integralMath;
 
 public class IntegralMath {
 
-    public static void one () {
-        double a = 2;
-        double b = 5;
-        double n = 100;
-
-        double s = 0;
-        double d = (b - a)/n;
-        double xb = a;
-
-        for(int i = 0; i < n; i++){
-            double xe = xb + d;
-            double xm = (xb + xe)/2;
-            s = s + d * Math.log(xm);
-            xb = xe;
-        }
-        System.out.println(" Интеграл 1  = " + s);
-    }
-
-    public static void two () {
-        double a = 3;
-        double b = 4;
-        double n = 100;
-
-        double s = 0;
-        double d = (b - a)/n;
-        double xb = a;
-
-        for(int i = 0; i < n; i++){
-            double xe = xb + d;
-            double xm = (xb + xe)/2;
-            s = s + d * ((xm * xm) + xm);
-            xb = xe;
-        }
-        System.out.println(" Интеграл 2  = " + s);
-    }
-
-    public static void three () {
-        double a = 0.01;
-        double b = 2;
-        double n = 100;
-
-        double s = 0;
-        double d = (b - a)/n;
-        double xb = a;
-
-        for(int i = 0; i < n; i++){
-            double xe = xb + d;
-            double xm = (xb + xe)/2;
-            s = s + d * Math.exp(- xm);
-            xb = xe;
-        }
-        System.out.println(" Интеграл 3  = " + s);
-    }
-
-    public static void four () {
-        double a = 1;
-        double b = 3;
-        double n = 100;
-
-        double s = 0;
-        double d = (b - a)/n;
-        double xb = a;
-
-        for(int i = 0; i < n; i++){
-            double xe = xb + d;
-            double xm = (xb + xe)/2;
-            s = s + d * Math.pow(xm, 2);
-            xb = xe;
-        }
-        System.out.println(" Интеграл 4  = " + s);
-    }
-
     public static void five () {
         double a = 0;
         double b = 1;
@@ -93,30 +21,39 @@ public class IntegralMath {
     }
 
     public static void main(String[] args) {
+        CalculationOptions one = new CalculateOne();
+        System.out.println("Первый интеграл: " + one.m(100));// Первое задание: вложенный класс
 
-    }
-    public static Double five () {
-        double a = 0;
-        double b = 1;
-        double n = 100;
+        CalculationOptions two = new CalculationOptions() {
+            @Override
+            public Double m(double n) {
+                double a = 3;
+                double b = 4;
+                double s = 0;
+                double d = (b - a)/n;
+                double xb = a;
 
-        double s = 0;
-        double d = (b - a)/n;
-        double xb = a;
+                for(int i = 0; i < n; i++){
+                    double xe = xb + d;
+                    double xm = (xb + xe)/2;
+                    s = s + d * ((xm * xm) + xm);
+                    xb = xe;
+                }
+                return s;
+            }
+        };
+        System.out.println("Второй интеграл: " + two.m(100));// Второе задание: анонимный класс
 
-        for(int i = 0; i < n; i++){
-            double xe = xb + d;
-            double xm = (xb + xe)/2;
-            s = s + d * xm * Math.sin(xm);
-            xb = xe;
-        }
-        return s;
-    }
+        System.out.println("Третий интеграл: " + new CalculateThree().m(100));// Третье задание: ссылка на статический метод (внутри класса CalculateThree)
 
-    private static class CalculatePrp implements CalculationOptions{
+        CalculateFour f = new CalculateFour();
+        CalculationOptions four = f::c;// ссылка на экземпляр класса
+        System.out.println("Четвертый интеграл: " + four.m(100));// Четвертое задание: ссылка на экземпляр класса
 
-        @Override
-        public Double m(double a, double b, double n) {
+        CalculationOptions five = n -> {// лямбда выражение (блоком)
+            double a = 0;
+            double b = 1;
+
             double s = 0;
             double d = (b - a)/n;
             double xb = a;
@@ -124,7 +61,66 @@ public class IntegralMath {
             for(int i = 0; i < n; i++){
                 double xe = xb + d;
                 double xm = (xb + xe)/2;
-                s = s + d * fun;
+                s = s + d * xm * Math.sin(xm);
+                xb = xe;
+            }
+            return s;
+        };
+        System.out.println("Пятый интеграл: " + five.m(100));// Пятое задание: лямбда-выражение
+    }
+
+    private static class CalculateOne implements CalculationOptions {
+
+        @Override
+        public Double m(double n) {
+            double a = 2;
+            double b = 5;
+            double s = 0;
+            double d = (b - a)/n;
+            double xb = a;
+
+            for(int i = 0; i < n; i++){
+                double xe = xb + d;
+                double xm = (xb + xe)/2;
+                s = s + d * Math.log(xm);
+                xb = xe;
+            }
+            return s;
+        }
+    }
+    private static class CalculateThree implements CalculationOptions {
+        CalculationOptions three = Math::exp;// ссылка на статический класс
+        @Override
+        public Double m(double n) {
+            double a = 0.01;
+            double b = 2;
+
+            double s = 0;
+            double d = (b - a)/n;
+            double xb = a;
+
+            for(int i = 0; i < n; i++){
+                double xe = xb + d;
+                double xm = (xb + xe)/2;
+                s = s + d * three.m(- xm);
+                xb = xe;
+            }
+            return s;
+        }
+    }
+    private static class CalculateFour {
+        double c (double n) {
+            double a = 1;
+            double b = 3;
+
+            double s = 0;
+            double d = (b - a)/n;
+            double xb = a;
+
+            for(int i = 0; i < n; i++){
+                double xe = xb + d;
+                double xm = (xb + xe)/2;
+                s = s + d * Math.pow(xm, 2);
                 xb = xe;
             }
             return s;
